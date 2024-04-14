c4e139754d89d4eb8bac97d2539026099f6494a6
Nikusha07 <129980489+Nikusha07@users.noreply.github.com>
2024-04-14 20:30:54 +0400

    Fix some bug
    *fix some bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,8 @@
-import clientPromise from "@/lib/mongodb"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth, { getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const adminEmails = ["nikatefnadze3@gmail.com"]
 
 export const authOptions = {
   providers: [
@@ -11,6 +12,25 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({session, token, user }) => {
+      if(adminEmails.includes(session?.user?.email)){
+        return session;
+      }else {
+        return "this email not can use admin panel"
+      }
+    },
+  },
 }
 
-export default NextAuth(authOptions)
+
+export default NextAuth(authOptions);
+
+
+export async function isAdminReq(req, res) {
+  const session = await getServerSession({ req, res });
+  if (!session || !session.user || !adminEmails.includes(session.user.email)) {
+    return false; 
+  }
+  return true;
+}
